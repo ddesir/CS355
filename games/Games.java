ffe8b22f14f82f65a72299ca88ad3d418d8d607c
ddesir <d.desir@gmail.com>
2019-02-17 10:11:35 -0500

    Corrected Poem, Guessing Game, and Fortune Teller games

--- games/Games.java
+++ games/Games.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 class Games {
 	static Random random = new Random();
-	static Scanner in = new Scanner(System.in);
+	// static Scanner in = new Scanner(System.in);
 	static String name;
+	// static int number;
 	static int guess;
 	
 	public Games() {
@@ -17,10 +18,12 @@ class Games {
 	}
 	
 	public static void set_name() {
+		Scanner in = new Scanner(System.in);
 		name = in.nextLine();
 	}
 	
 	public static void set_guess() {
+		Scanner in = new Scanner(System.in);
 		guess = in.nextInt();
 	}
 	
@@ -30,28 +33,20 @@ class Games {
 	
 	// Poem
 	public static String poem() {
-		System.out.println("What is your name? ");
-		set_name();
 		return "My student " + get_name() + ",\nstanding proud,\nis a fine example for the crowd.";
 	}
 	
 	// Guessing Game
 	public static String guessing_game() {
 		int number = (int)(random.nextInt(100 + 1));
-		System.out.print("Enter your guess: ");
-    set_guess();
-
-    while (guess != number) {
-      if (guess < number) {
-        System.out.println("Guess was too low");
-        set_guess();
-      } else if (guess > number) {
-        System.out.println("Guess was too high");
-        set_guess();
-      } else {
-        break;
-      }
+		guess = get_guess();
+		
+		while (get_guess() != number) {
+			String not_equal = (guess > number) ? "Guess was too high" : "Guess was too low";
+			System.out.print(not_equal + "\nGuess again: ");
+			set_guess();
     }
+
     return "Correct, the number was " + number;
 	}
 	
@@ -59,25 +54,110 @@ class Games {
 	public static String fortune_teller() {
 		String[] fortunes = {
       "You're not successful unless you have a few haters.",
-      "be present in the moment.", "Nothing is consistent but change.",
+      "Be present in the moment.", "Nothing is consistent but change.",
       "The ends will justify the means.", "Things change. Friends leave." +
       "Life doesn't stop for anybody."
     };
 
-    int index = (int)(random.nextInt(fortunes.length - 1));
-    System.out.print("Enter your name: ");
-    set_name();
+    int number = (int)(random.nextInt(fortunes.length - 1));
     
-    return "Here is your fortune " + get_name() + ":\n" + fortunes[index];
+    return "Here is your fortune " + get_name() + ":\n" + fortunes[number];
 	}
 	
 	// Madlibs
-	public static String madlibs() {
+	/* 
+public static String madlibs(String adj1, String pnoun1, int num1, String pnoun2, String adj2, String adv1, String adj3, String noun1, String verb, String body_part1, String person, String adj4, int num2, String body_part2, String adv2, String adj5, String pnoun3, String adv3, String pnoun4, String noun2, String body_part3) {
+		String adj1;
+		String pnoun1;
+		int num1;
+		String pnoun2;
+		String adj2;
+		String adv1;
+		String adj3;
+		String noun1;
+		String verb;
+		String body_part1;
+		String person;
+		String adj4;
+		int num2;
+		String body_part2;
+		String adv2;
+		String adj5;
+		String pnoun3;
+		String adv3;
+		String pnoun4;
+		String noun2;
+		String body_part3;
+		
 		String lets_dance = 
-			"At my {adj1} sleepover party, my best {pnoun1} and I decided to have a dance-off. We made my {num1}-year-old little sister be the judge. We broke into two teams, \"The {pnoun2}\" and \"The {adj2} Dancers.\" My team danced {adv1}, but the other team's {adj3} moves were out of this {noun1}! They totally out-";
+			"At my #{adj1} sleepover party, my best #{pnoun1} and I decided to have a dance-off. We made my #{num1}-year-old little sister be the judge. We broke into two teams, \"The #{pnoun2}\" and \"The #{adj2} Dancers.\" My team danced #{adv1}, but the other team's #{adj3} moves were out of this #{noun1}! They totally out-#{verb} us. So when no one was looking, I grabbed my sister by the #{body_part1} and pulled her aside. \"#{person},\" I whispered, \"I promise to do all of your #{adj4} chores for #{num2} months if you say that my team won.\" My sister shook her #{body_part2}. \"No way!\" she said #{adv2}. \"Your team danced worse than a bunch of #{adj5} #{pnoun3}!\" \"Fine,\" I said. \"Then I'll just have to tell all of my friends that you're #{adv3} afraid of #{pnoun4}.\" That helped to change her #{noun2}. We won that contest, #{body_part3} down!";
 			
-			return "";
+			System.out.println("Enter an adjective: ");
+			adj1 = in.next();
+			
+			System.out.println("Enter an plural noun: ");
+			pnoun1 = in.next();
+			
+			System.out.println("Enter an number: ");
+			num1 = in.nextInt();
+			
+			System.out.println("Enter an plural noun: ");
+			pnoun2 = in.next();
+			
+			System.out.println("Enter an adjective: ");
+			adj2 = in.next();
+			
+			System.out.println("Enter an adverb: ");
+			adv1 = in.next();
+			
+			System.out.println("Enter an adjective: ");
+			adj3 = in.next();
+			
+			System.out.println("Enter an noun: ");
+			noun1 = in.next();
+			
+			System.out.println("");
+			verb = in.next();
+			
+			System.out.println("");
+			body_part1 = in.next();
+			
+			System.out.println("");
+			person = in.next();
+			
+			System.out.println("Enter an adjective: ");
+			adj4 = in.next();
+			
+			System.out.println("Enter an number: ");
+			num2 = in.nextInt();
+			
+			System.out.println("");
+			body_part2 = in.next();
+			
+			System.out.println("Enter an adverb: ");
+			adv2 = in.next();
+			
+			System.out.println("Enter an adjective: ");
+			adj5 = in.next();
+			
+			System.out.println("Enter an plural noun: ");
+			pnoun3 = in.next();
+			
+			System.out.println("Enter an adverb: ");
+			adv3 = in.next();
+			
+			System.out.println("Enter an plural noun: ");
+			pnoun4 = in.next();
+			
+			System.out.println("Enter an noun: ");
+			noun2 = in.next();
+			
+			System.out.println("");
+			body_part3 = in.next();
+			
+			return String.format(lets_dance, adj1, pnoun1, num1, pnoun2, adj2, adv1, adj3, noun1, verb, body_part1, person, adj4, num2, body_part2, adv2, adj5, pnoun3, adv3, pnoun4, noun2, body_part3);
 	}
+ */
 
 
 }
--- games/Main.java
+++ games/Main.java
@@ -1,19 +1,23 @@
 import java.util.*;
 
 class Main extends Games {
+	static Scanner in = new Scanner(System.in);
+	
 	public static void main(String [] args) {
 		String output = "";
 		System.out.print(
 			"WELCOME\n" +
 			"1 - Poem\n" +
-			"2 - Fortune Teller\n" +
-			"3 - MadLibs\n\n" +
+			"2 - Guessing Game\n" +
+			"3 - Fortune Teller\n" +
+			"4 - MadLibs\n\n" +
 			"Choose a game: ");
 		
 		int choice = in.nextInt();
 		// System.out.println(fortune_teller());
 		
-		if (choice == 1) {
+		/* 
+if (choice == 1) {
 			output = poem();
 		} else if (choice == 2) {
 			output = guessing_game();
@@ -27,6 +31,34 @@ class Main extends Games {
 		}
 		
 		System.out.println(output);
+ */
+		
+		switch (choice){
+			case 1:
+				System.out.println("What is your name? ");
+				set_name();
+				System.out.print(poem());
+				System.out.println("\n");
+				break;
+			case 2:
+				System.out.print("Enter your guess: ");
+				set_guess();
+    		
+				System.out.print(guessing_game());
+				System.out.println("\n");
+				break;
+			case 3:
+				 System.out.print("Enter your name: ");
+				 set_name();
+				 System.out.print(fortune_teller());
+				 System.out.println("\n");
+			case 4:
+				break;
+			default:
+				System.out.println("You've entered an invalid choice.\nChoose again: ");
+				choice = in.nextInt();
+				break;
+		}
 				
 	}
 }

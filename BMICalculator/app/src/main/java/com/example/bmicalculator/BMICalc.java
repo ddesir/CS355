8f351c847de94713b4f32126fc4912e5d182f18e
ddesir <d.desir@gmail.com>
2019-03-06 11:53:04 -0500

    Initial commit

--- BMICalculator/app/src/main/java/com/example/bmicalculator/BMICalc.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/BMICalc.java
@@ -1,4 +1,23 @@
 package com.example.bmicalculator;
 
 public class BMICalc {
+//    private static float height, weight;
+
+    BMICalc() {
+
+    }
+
+/*    public static void setHeight(float x) {
+        height = x;
+    }
+
+    public static void setWeight(float y) {
+        weight = y;
+    } */
+
+    public static float calcultateBMI(float x, float y) {
+        Float height = Float.parseFloat(x);
+        Float weight = Float.parseFloat(y);
+        return Math.pow(height, 2)/weight;
+    }
 }
--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.bmicalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.bmicalculator.BMICalc.calcultateBMI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayResult (View view) {
+        Intent intent = new Intent (this, DisplayMessageActivity.class);
+        EditText ht = (EditText) findViewById(R.id.editText);
+        EditText wt = (EditText) findViewById(R.id.editText2);
+        float height = ht.getText().toString();
+        float weight = wt.getText().toString();
+        String message = BMICalc.calcultateBMI(height, weight);
+        intent.putExtra("msg1", message);
+        startActivity(intent);
     }
 }

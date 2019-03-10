d153aa73cabfcb1322dcf4ed2ccce9750b6a962a
ddesir <d.desir@gmail.com>
2019-03-10 11:25:14 -0400

    BMI Calculator complete

--- BMICalculator/app/src/main/java/com/example/bmicalculator/BMICalc.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/BMICalc.java
@@ -1,5 +1,8 @@
 package com.example.bmicalculator;
 
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
+
 public class BMICalc {
 //    private static float height, weight;
 
@@ -7,15 +10,19 @@ public class BMICalc {
 
     }
 
-/*    public static void setHeight(float x) {
-        height = x;
-    }
+    public static String calcultateBMI(float height, float weight) {
+        double bmi = weight / (Math.pow(height, 2))*703;
+        NumberFormat formatter = new DecimalFormat("#0.0");
 
-    public static void setWeight(float y) {
-        weight = y;
-    } */
+        if (bmi < 18.5) {
+            return "Your BMI is " + formatter.format(bmi) + ".\nYou are underweight.";
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            return "Your BMI is " + formatter.format(bmi) + ".\nYou are healthy.";
+        } else if (bmi >= 25.0 && bmi <= 29.9) {
+            return "Your BMI is " + formatter.format(bmi) + ".\nYou are overweight.";
+        } else {
+            return "Your BMI is " + formatter.format(bmi) + ".\nYou are obese.";
+        }
 
-    public static float calcultateBMI(float height, float weight) {
-        return Math.pow(height, 2)/weight;
-    }
+        }
 }
--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import static com.example.bmicalculator.BMICalc.calcultateBMI;
 
 public class MainActivity extends AppCompatActivity {
 

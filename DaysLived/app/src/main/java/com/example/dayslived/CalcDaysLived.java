64ac239b13d00ecd72613fd9b6479866d0c6b6f9
ddesir <d.desir@gmail.com>
2019-03-30 18:44:35 -0400

    Added GFG/Date class from Geeks for Geeks

--- DaysLived/app/src/main/java/com/example/dayslived/CalcDaysLived.java
+++ DaysLived/app/src/main/java/com/example/dayslived/CalcDaysLived.java
@@ -7,7 +7,6 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class CalcDaysLived extends AppCompatActivity {
 
@@ -20,26 +19,26 @@ public class CalcDaysLived extends AppCompatActivity {
     public void calculateDays(View view) {
         // Retrieve values from date picker
         DatePicker p = (DatePicker) findViewById(R.id.datePicker);
-        int day = p.getDayOfMonth();
-        int month = p.getMonth();
-        int year = p.getYear();
+        int bDay = p.getDayOfMonth();
+        int bMonth = p.getMonth() + 1;
+        int bYear = p.getYear();
 
         // Get current date
         Calendar c = Calendar.getInstance();
-        int currentDay = c.get(Calendar.DAY_OF_MONTH);
-        int currentMonth = c.get(Calendar.MONTH) + 1;
-        int currentYear = c.get(Calendar.YEAR);
+        int thisDay = c.get(Calendar.DAY_OF_MONTH);
+        int thisMonth = c.get(Calendar.MONTH) + 1;
+        int thisYear = c.get(Calendar.YEAR);
 
-        Date past = new Date(year, month, day);
-        Date today = new Date(currentYear, currentMonth, currentDay);
+        GFG.Date dt1 = new GFG.Date(bDay, bMonth, bYear);
+        GFG.Date dt2 = new GFG.Date(thisDay, thisMonth, thisYear);
+        int lived = GFG.getDifference(dt1, dt2);
 
-        // int days = today - past;
-
-        String currentDate = String.format("Today is %d/%d/%d", currentMonth, currentDay, currentYear);
-        // String lived = String.format("You have lived for %d days!!!", days);
+        String currentDate = String.format("Today is %d/%d/%d.", thisMonth, thisDay, thisYear);
+        String birthDate = String.format("\nYou were born on %d/%d/%d.", bMonth, bDay, bYear);
+        String alive = String.format("\nYou have been alive for %d days.", lived);
 
         // Change text on TextView
         TextView tv = (TextView) findViewById(R.id.textView2);
-        tv.setText(String.format(currentDate));
+        tv.setText(currentDate + birthDate + alive);
     }
 }

8f3f42147390b59bd21e543cc805cdb8e9645376
ddesir <d.desir@gmail.com>
2019-04-08 10:08:01 -0400

    Created views, intents, received intents

--- SQL/app/src/main/java/com/example/sql/Listing.java
+++ SQL/app/src/main/java/com/example/sql/Listing.java
@@ -1,7 +1,16 @@
 package com.example.sql;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Listing extends AppCompatActivity {
 
@@ -9,5 +18,46 @@ public class Listing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
+
+        // Retrieve argument sent from MainActivity
+        Intent intent = getIntent();
+        String q = intent.getStringExtra("query");
+
+        // Query the DB and store the results in an ArrayList
+        ArrayList<String> values = queryDB(q);
+
+        // Get ListView component and display the results of the query
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        lv.setAdapter(adp);
     }
+
+    ArrayList<String> queryDB (String query) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        try {
+            SQLiteDatabase db = openOrCreateDatabase("foodProducts.db", Context.MODE_PRIVATE, null);
+            Cursor cursor = db.rawQuery(query, null);
+
+            while (cursor.moveToNext()) {
+                StringBuilder data = new StringBuilder();
+
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    if (i > 0) data.append(" || ");
+                    String colvalue = cursor.getString(i);
+                    data.append(colvalue);
+                }
+
+                list.add(data.toString());
+            }
+
+            cursor.close();
+            db.close();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error opening / querying Database", Toast.LENGTH_LONG).show();
+        }
+
+        return list;
+    }
+
 }
